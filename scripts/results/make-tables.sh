5afa5dcc01f319251d94eee00bdaf66e576b5879
Jack McKeown <jackamckeown@gmail.com>
2024-04-01 13:09:08 -0400

    idk

--- scripts/results/make-tables.sh
+++ scripts/results/make-tables.sh
@@ -4,12 +4,16 @@
 
 echo "Making MPT tables / figures"
 ~/.pyenv/shims/python scripts/results/compareSolved.py MPTAuto MPTRoundRobin MPTRoundRobinAllOnes MPTNN1Hist MPTConstCat1Hist MPTConstCatDistilled_gain5_  --cv
+
+echo "Making MPT ConstCat heatmaps"
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTConstCat1Hist0 MPTConstCat1Hist0 --problem=MPT0557+1.p --dataset=MPT
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTConstCat1Hist1 MPTConstCat1Hist1 --problem=MPT1964+1.p --dataset=MPT
 
+echo "Making MPT NN heatmaps"
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTNN1Hist0 MPTNN1Hist0 --problem=MPT1592+1.p --dataset=MPT
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTNN1Hist0 MPTNN1Hist0 --problem=MPT0557+1.p --dataset=MPT
 
+echo "Making more MPT NN heatmaps"
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTNN1Hist1 MPTNN1Hist1 --problem=MPT1539+1.p --dataset=MPT
 ~/.pyenv/shims/python scripts/graphs/policyHeatmap.py MPTNN1Hist1 MPTNN1Hist1 --problem=MPT1964+1.p --dataset=MPT
 
@@ -20,4 +24,5 @@ echo "Making VBT tables / figures"
 echo "Making SLH tables / figures"
 ~/.pyenv/shims/python scripts/results/compareSolved.py SLHAuto SLHRoundRobin SLHRoundRobinAllOnes SLHNN1Hist SLHConstCat1Hist SLHConstCatDistilled_gain5_  --cv
 
+echo "Finished!"
 
