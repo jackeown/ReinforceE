4f037c4cd3bde89e6ce28773fa612a94bf0e6979
Jack McKeown <jackamckeown@gmail.com>
2024-05-20 13:50:58 -0400

    updated scripts

--- scripts/graphs/autoStratsVis.py
+++ scripts/graphs/autoStratsVis.py
@@ -179,6 +179,7 @@ if __name__ == "__main__":
         print(f"Loading {len(histFiles)} files...")
         from joblib import Parallel, delayed
         hists = Parallel(n_jobs=20,verbose=13)(delayed(ECallerHistory.load)(name(x), keysToDeleteFromInfos={"stdout", "stderr", "states", "actions", "rewards"}) for x in histFiles)
+        hists = {name(x):h for x,h in zip(histFiles, hists)}
 
         hists = mergeHists(hists)
         plotHeatmap(makeHeatmap(args, hists), args.aspect, extraVlines=extraVlines)
